6a5f770ec250a2aef1906fe636c6dcc71f44673d
zakumann <samuel742able@gmail.com>
2022-10-02 23:17:27 +0900

    파일 정리

--- coding_interview/03_연필_개수.js
+++
@@ -1,6 +0,0 @@
-function solution(n) {
-    let answer = Math.ceil(n/12)
-
-    return answer;
-}
-console.log(solution(178));
--- coding_interview/04_1부터_N까지_합_출력하기.js
+++
@@ -1,8 +0,0 @@
-function solution(n){
-    let answer=0;
-    for(let i =1;i<=n;i++){
-        answer=answer+i;
-    }
-    return answer;
-}
-console.log(solution(10));
