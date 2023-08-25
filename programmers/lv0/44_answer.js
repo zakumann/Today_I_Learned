a9ac526a26c585724dd736d93fed89a3eae31c52
zakumann <samuel742able@gmail.com>
2023-08-25 22:40:18 +0900

    fix the wrong typing

--- programmers/lv0/44_answer.js
+++ programmers/lv0/44_answer.js
@@ -19,14 +19,6 @@ function solution(my_string, queries) {
 
 /*
 
-2
-3
-4
-5
-6
-7
-8
-9
 function solution(my_string, queries) {
     let str = my_string.split('');
   queries.forEach(([start, end]) => {
